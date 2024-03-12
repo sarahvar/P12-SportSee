48fe1add3fedd6248ddc0afe001d8b4a3d2307ca
sarahvar <sarahvarin95@gmail.com>
2024-03-12 19:17:02 +0100

    ErrorPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,16 @@ import TopNavBar from "./components/TopNav/TopNav";
 import ErrorPage from "./pages/Error/Error";
 import HomePage from "./pages/Home/Home";
 
-
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <TopNavBar>
-          <Routes>
-            <Route path="*" element={<ErrorPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </TopNavBar>
-        <SideNavBar/>
-        <HomePage/>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <TopNavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <SideNavBar />
+    </BrowserRouter>
   );
 }
 
