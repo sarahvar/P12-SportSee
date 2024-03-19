420f75c983a5e1b396767bb09861de3b9cdf5228
sarahvar <sarahvarin95@gmail.com>
2024-03-19 17:12:32 +0100

    User

---
+++ src/pages/User/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderDashboard from "../../components/Dashboard/HeaderDashboard/HeaderDashboard";
+import Activity from "../../components/Dashboard/ActivityCharts/Activity";
+
+
+const User = () => {
+  return (
+    <div>
+      <HeaderDashboard />
+      <Activity />
+    </div>
+  );
+}
+
+export default User;
