c5d0aad97d53ab60a424e43785e23dfbce1f8dc7
sarahvar <sarahvarin95@gmail.com>
2024-04-06 16:57:25 +0200

    console.log_api

--- src/service/dataSwitch.js
+++ src/service/dataSwitch.js
@@ -1,9 +1,13 @@
+
 export const getData = () => {
-  const useMockData = import.meta.env.VITE_USE_MOCK_DATA === 'false';
+  const useMockData = import.meta.env.VITE_USE_MOCK_DATA === 'true';
 
   if (useMockData) {
+    console.log("Utilisation des données mockées");
     return 'mocked'; // Utilisation des données mockées
   } else {
+    console.log("Utilisation des données de l'API");
     return 'api'; // Utilisation des données de l'API
   }
 };
+
